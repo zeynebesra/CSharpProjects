3b1040d3ee9ced9b8d6fc650f90ce3c0dedaa875
Zeyneb Esra Öztürk <75810064+zeynebesra@users.noreply.github.com>
2023-04-25 13:50:56 +0300

    RSS ile İnternetten Haber Başlıkları Çekme eklendi.

---
+++ RSS_Veri_Cekme/RSS_Veri_Cekme/Form1.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Xml;
+
+namespace RSS_Veri_Cekme
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        void haberiCek(string link)
+        {
+            listBox1.Items.Clear();
+            XmlTextReader xmloku = new XmlTextReader(link);
+            while (xmloku.Read())
+            {
+                if (xmloku.Name == "title")
+                {
+                    listBox1.Items.Add(xmloku.ReadString());
+                }
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            haberiCek("https://www.cnnturk.com/feed/rss/turkiye/news");
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            haberiCek("https://feeds.bbci.co.uk/turkce/rss.xml");
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            haberiCek("https://www.gzt.com/rss");
+        }
+    }
+}
